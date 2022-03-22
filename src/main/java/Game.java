497ba85fbe8d57657cec26297f799df2ab24a02a
jabolins <juris.abolins.cs@gmail.com>
2022-03-22 14:24:41 +0200

    Initial commit

---
+++ src/main/java/Game.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Game {
+    Scanner scanner;
+
+    public String menu(){
+      String choose= null;
+      while (choose !="W")
+        return "d";
+    }
+}
